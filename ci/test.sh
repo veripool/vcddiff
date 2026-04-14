58245d714e475e4b7b082655e12678a5297e1e65
Todd Strader <tstrader@hudson-trading.com>
2026-04-14 12:01:26 -0400

    ident mapping bug

--- ci/test.sh
+++ ci/test.sh
@@ -7,19 +7,19 @@ set -e
 set -x
 
 if [[ "$(uname -s)" != MINGW* && "$(uname -s)" != MSYS* && "$(uname -s)" != CYGWIN* ]]; then
-  FIFO_SUPPORTED=1
+    FIFO_SUPPORTED=1
 else
-  FIFO_SUPPORTED=0
+    FIFO_SUPPORTED=0
 fi
 
-! ./vcddiff tests/counter.vcd tests/counter.vcd | grep -q .
-! ./vcddiff tests/counter.vcd tests/counter.time.no_diff.vcd | grep -q .
-! ./vcddiff tests/counter.vcd tests/counter.change_reorder.no_diff.vcd | grep -q .
-! ./vcddiff tests/counter.vcd tests/counter.var_reorder.no_diff.vcd | grep -q .
-! ./vcddiff tests/counter.vcd tests/counter.identifier.no_diff.vcd | grep -q .
-! ./vcddiff tests/counter.vcd tests/counter.scope_move.no_diff.vcd | grep -q .
+test -z "$(./vcddiff tests/counter.vcd tests/counter.vcd)"
+test -z "$(./vcddiff tests/counter.vcd tests/counter.time.no_diff.vcd)"
+test -z "$(./vcddiff tests/counter.vcd tests/counter.change_reorder.vcd)"
+test -z "$(./vcddiff tests/counter.vcd tests/counter.var_reorder.vcd)"
+test -z "$(./vcddiff tests/counter.vcd tests/counter.identifier.vcd)"
+test -z "$(./vcddiff tests/counter.vcd tests/counter.scope_move.no_diff.vcd)"
 if [ "$FIFO_SUPPORTED" = "1" ]; then
-  ! ./vcddiff <(cat tests/counter.vcd) <(cat tests/counter.vcd) | grep -q .
+    test -z "$(./vcddiff <(cat tests/counter.vcd) <(cat tests/counter.vcd))"
 fi
 
 ./vcddiff tests/counter.vcd tests/counter.end_time.diff.vcd |
@@ -31,9 +31,13 @@ fi
 ./vcddiff tests/counter.vcd tests/counter.new_sig.diff.vcd |
     grep "Ignoring signal t.the_sub.new_sig .* - not defined in both files"
 if [ "$FIFO_SUPPORTED" = "1" ]; then
-  ./vcddiff <(cat tests/counter.vcd) <(cat tests/counter.end_time.diff.vcd) |
-      grep "Files have different end times"
+    ./vcddiff <(cat tests/counter.vcd) <(cat tests/counter.end_time.diff.vcd) |
+        grep "Files have different end times"
 fi
+
+# Bug: vcddiff reports false differences when identifiers are shared across
+# scopes in one file but unique in the other (wavediff handles this correctly)
+test -z "$(./vcddiff tests/idcode_a.vcd tests/idcode_b.vcd)"
 
 ./vcddiff --version |
     grep "vcddiff"
